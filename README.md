0229dc6fa381afa1b477422cfae7ffc92899f4d6
FiZaRafakat <164252341+FiZaRafakat@users.noreply.github.com>
2024-04-01 15:04:29 -0700

    designing

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# CLI-Number-Guessing-Game
+This is a simple number guessing game where you can enjoy playing with your PC.
+<br/>
+The logic of this game is such that we will guess a number and at the same time , our PC will generate a number. If your number matches the generated number, we win the  game; otherwise , you will be given three chances to guess the correct number. Additionally a bit of designing has been done using chalk here.
+<br/>
--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
-console.log(chalk.bold.italic.blue("Welcome To The Fiza's Number Guessing game"));
+import clear from "clear";
+clear();
+console.log(chalk.bold.italic.magentaBright("\n \t Welcome To The Fiza's Number Guessing game \n"));
 const randomNumber = Math.floor(Math.random() * 10 + 1);
-const answers = await inquirer.prompt([
-    {
-        name: "userGuessedNumber",
-        type: "number",
-        message: "Please Guess a Number between 1-10 : ",
-    },
-]);
-if (answers.userGuessedNumber === randomNumber) {
-    console.log(chalk.bold.yellowBright("CONGRATULATIONS! You guess right number."));
-    process.exit();
-}
-else {
-    console.log(chalk.bold.red(" SORRY! You guess wrong number. TRY AGAIN "));
+let userLife = 3;
+while (true) {
+    console.log(chalk.underline.green(userLife + " Lives Remaining \n"));
+    if (userLife !== 0) {
+        const answers = await inquirer.prompt([
+            {
+                name: "userGuessedNumber",
+                type: "number",
+                message: "Please Guess a Number between 1-10 : ",
+            },
+        ]);
+        if (answers.userGuessedNumber > 0 && answers.userGuessedNumber <= 10) {
+            const difference = Math.abs(randomNumber - answers.userGuessedNumber);
+            if (difference === 0) {
+                console.log(chalk.italic.yellowBright("\n \tCONGRATULATIONS! You guess right number.\n"));
+                break;
+            }
+            else if (difference < 2) {
+                console.log(chalk.bold.black.italic("\n \tSo close! Just one number away\n"));
+                userLife--;
+            }
+            else if (difference <= 2) {
+                console.log(chalk.bold.italic.black("\n \t You are close Keep going!\n"));
+                userLife--;
+            }
+            else {
+                console.log(chalk.bold.italic.black("\n \t You are far away.Try Again\n"));
+                userLife--;
+            }
+        }
+        else {
+            console.log(chalk.red.bold("\n Please Enter a valid number within the given range\n"));
+        }
+    }
+    else {
+        console.log(chalk.italic.red("\n \t Awwww! Game over... The number was " + randomNumber + "\n"));
+        break;
+    }
 }
